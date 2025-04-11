1c98582e36b69677b46ab65317233a09db6c4ee0
berru <berruhanedar@icloud.com>
2025-04-11 14:51:47 +0300

    Java Switch Case

---
+++ src/_07_Switch/Switch.java
@@ -0,0 +1,37 @@
+package _07_Switch;
+
+public class Switch {
+    public static void main(String[] args) {
+
+        //  Java reaches a break keyword, it breaks out of the switch block.
+        // The default keyword specifies some code to run if there is no case match
+
+        int day = 4;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day");
+                break;
+        }
+    }
+}
