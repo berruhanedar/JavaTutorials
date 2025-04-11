ae5c3cc7c3a90d379844666d1c2385fe46714f5c
berru <berruhanedar@icloud.com>
2025-04-11 14:51:30 +0300

    Java Operators

---
+++ src/_04_JavaOperators/JavaMathOperators.java
@@ -0,0 +1,34 @@
+package _04_JavaOperators;
+
+public class JavaMathOperators {
+
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 3;
+
+        // Addition
+        System.out.println(x + y);
+
+        // Substraction
+        System.out.println(x - y);
+
+        // Multiplication
+        System.out.println(x * y);
+
+        // Division
+        System.out.println(x / y);
+
+        // Modulus
+        System.out.println(x % y);
+
+        // Increment
+        int z = 7;
+        ++z;
+        System.out.println(z);
+
+        // Decrement
+        int v = 7;
+        --v;
+        System.out.println(v);
+    }
+}
---
+++ src/_04_JavaOperators/StringConcatenation.java
@@ -0,0 +1,11 @@
+package _04_JavaOperators;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
+
