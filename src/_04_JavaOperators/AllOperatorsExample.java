17879c9b5069df3873d068169ee13d8b8933fc6f
berru <berruhanedar@icloud.com>
2025-04-11 14:50:11 +0300

    Java Operators

---
+++ src/_04_JavaOperators/AllOperatorsExample.java
@@ -0,0 +1,28 @@
+package _04_JavaOperators;
+
+public class AllOperatorsExample {
+    public static void main(String[] args) {
+        int result1 = 100 + 50; // Addition
+        System.out.println("result1 = " + result1);
+
+        int result2 = 100 - 50; // Substraction
+        System.out.println("result2 = " + result2);
+
+        int result3 = 100 * 50; // Multiplication
+        System.out.println("result3 = " + result3);
+
+        int result4 = 100 / 50; // Division
+        System.out.println("result4 = " + result4);
+
+        int result5 = 5 % 2; // Modulus
+        System.out.println("result5 = " + result5);
+
+        int result6 = 5; // Increment
+        result6++;
+        System.out.println("result6 = " + result6);
+
+        int result7 = 5;
+        result7--;
+        System.out.println("result7 = " + result7);
+    }
+}
