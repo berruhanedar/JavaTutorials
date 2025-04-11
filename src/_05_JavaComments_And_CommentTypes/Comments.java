0d85d580b1c6904eb5dcb4ed0badd31327e2616b
berru <berruhanedar@icloud.com>
2025-04-11 14:50:49 +0300

    Java Comments

---
+++ src/_05_JavaComments_And_CommentTypes/Comments.java
@@ -0,0 +1,13 @@
+package _05_JavaComments_And_CommentTypes;
+
+public class Comments {
+
+    // THIS IS A JAVA SINGLE-LINE COMMENTS
+
+    /* THIS IS
+    A JAVA
+    MULTI-LINE
+    COMMENTS
+    */
+
+}
